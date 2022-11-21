a85f93988809f9de5a5bd727ac2b1b76c2e873a5
Irina Gayfulina <63535542+gayfulina@users.noreply.github.com>
2022-11-21 13:19:06 -0800

    fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -14,7 +14,9 @@
 
 function fizzBuzz(n) {
     for (let i=1; i<=n; i++) {
-        if (i % 3 === 0 && i % 5 === 0 ) {
+        // if (i % 3 === 0 && i % 5 === 0 ) {
+        //or
+        if (i % 15 === 0) {
             console.log('fizzbuzz')
         } else if (i % 5 === 0) {
             console.log('buzz')
