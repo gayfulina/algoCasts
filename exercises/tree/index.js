097badc2b0345315ecee07053f4fceedf73abd24
Irina Gayfulina <63535542+gayfulina@users.noreply.github.com>
2023-01-20 11:49:37 -0800

    tree fn

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -20,13 +20,15 @@ class Node {
         this.children.push(new Node(data));
     }
 
-    remove (data) {
-       this.children = this.children.filter(node => node.data !== data)
+    remove(data) {
+        this.children = this.children.filter(node => node.data !== data)
     }
 }
 
 class Tree {
-
+    constructor() {
+        this.root = null;
+    }
 }
 
-module.exports = { Tree, Node };
+module.exports = {Tree, Node};
