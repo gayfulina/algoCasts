e7f236f66d85795129fd56a19565452268cc5a89
Irina Gayfulina <63535542+gayfulina@users.noreply.github.com>
2023-01-20 12:43:24 -0800

    df traversal

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -41,6 +41,17 @@ class Tree {
         }
     }
 
+    traverseDF (fn) {
+        const arr = [this.root];
+
+        while (arr.length) {
+            const node = arr.shift();
+
+            arr.unshift(...node.children);
+            fn(node)
+        }
+    }
+
 }
 
 module.exports = {Tree, Node};
